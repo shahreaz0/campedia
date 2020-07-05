7c8a3ab3c42a6e8fa010b36a260bdad2a5c3e131
shahreaz0 <ashahreaz@gmail.com>
2020-07-05 07:27:27 +0600

    Camp image upload

--- src/routes/camp.js
+++ src/routes/camp.js
@@ -1,21 +1,36 @@
 const router = require("express").Router();
+const path = require("path");
+const multer = require("multer");
 
-const camps = [
-	{ name: "Lake Paradise", imageUrl: "http://lake.com" },
-	{ name: "Lake Paradise", imageUrl: "http://lake.com" },
-	{ name: "Lake Paradise", imageUrl: "http://lake.com" },
-];
+const camps = [];
+
+// multer config
+const upload = multer({
+	dest: path.join("public", "img", "campImg"),
+	limits: 100000,
+	fileFilter(req, file, cb) {
+		if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+			cb(new Error("File type not allowed."));
+		}
+		cb(null, true);
+	},
+});
 
 router.get("/camps", (req, res) => {
 	res.render("camps", { pageTitle: "Camps", camps });
 });
 
-router.post("/camps", (req, res) => {
-	res.send("/camps post");
+router.post("/camps", upload.single("campImg"), (req, res) => {
+	camps.push({
+		name: req.body.campName,
+		imageUrl: "/" + path.join("img", "campImg", req.file.filename),
+	});
+	console.log(camps);
+	res.redirect("/");
 });
 
 router.get("/camps/new", (req, res) => {
-	res.send("/camps/new");
+	res.render("new", { pageTitle: "Add Camp" });
 });
 
 module.exports = router;
