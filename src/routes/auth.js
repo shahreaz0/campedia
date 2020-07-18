a3350d7344dcac132e0963ba9c2ead9ff324cc8b
shahreaz0 <ashahreaz@gmail.com>
2020-07-18 21:58:48 +0600

    Auth routes in navbar

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,20 @@
 const router = require("express").Router();
 
 // get /register shows register form
-
+router.get("/register", (req, res) => {
+	res.send("show register form");
+});
 // post /register
-
+router.post("/register", (req, res) => {
+	res.send("/register");
+});
 // get /login shows login form
-
+router.get("/login", (req, res) => {
+	res.send("get login form");
+});
 // post /login
+router.post("/login", (req, res) => {
+	res.send("post /login");
+});
 
 module.exports = router;
